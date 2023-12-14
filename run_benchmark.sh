e6f254676ca4226c61c48c9e0efbf200157b0580
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2023-12-14 10:00:23 +0100

    benchmark runner script

---
+++ run_benchmark.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+N=100
+
+rm -rf ./results
+mkdir ./results
+
+echo "Starting"
+./node_exporter > results/node_exporter.log 2>&1 &
+PID=$!
+sleep 5
+
+for i in $(seq 1 $N)
+do
+    echo "Request $i"
+    curl localhost:9100/metrics >> results/benchmark.log
+    sleep 1
+done
+
+kill $PID
+
